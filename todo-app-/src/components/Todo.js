b05053eed4f3a2c5c8c6bae12a65c96d0463206a
Fathima Shafrin <shafrinpt@gmail.com>
2025-07-11 16:23:16 +0530

    ui still pending

--- todo-app-/src/components/Todo.js
+++ todo-app-/src/components/Todo.js
@@ -5,48 +5,60 @@ import Cards from'./assets/bg-card-back.png'
 import './Todo.css'
 const Todo = () => {
  
-  const[submit,setSubmit]=useState('')
-
-  const handleChange = (e)=>{
-   setSubmit(e.target.value)
-  }
-
+  const[name,setName]=useState('')
+  const[cardNum,setCardNum]=useState("")
+  const[cal,setCal]=useState("")
+  const[cvc,setCvc]=useState("")
+ 
   return (
     <div className='container' >
-      
-      <form className='form'>
-
+      <section>
       <div className='card'>
         <img className='img1' src={myImg} />
      </div>
+      <form className='form'>
+
+      
 
 
-        <img className='img2' src={Card} />
-        <img className='img2' src={Cards} />
+        <img className='front' src={Card} />
+        <img className='back' src={Cards} />
   
 
 
       <div className='inputs'>
-
-      <label for='chardholder-name'>CHARDHOLDER NAME:</label>
-       <input id='chardholder-name' value={submit} type='text' placeholder='e.g fathima' onChange={handleChange} />  
-       <label for='card-number'>CARD NUMBER:</label>
-       <input id='card-number' type='text'  placeholder='e.g 1234567890'/>
-      <span>
-       <label for='exp'>EXP.DATE(MM/YY) </label>
-       <label for='cvc'>CVC</label></span>
-       <span>
-       <input id='exp' type='date'/>
-      <input id='cvc'  type='text' placeholder='e.g 123'/>
-
-       </span><br></br>
+       
+      <label htmlFor='chardholder-name'>CHARDHOLDER NAME:</label>
+       <input id='chardholder-name' value={name} type='text' placeholder='e.g fathima' onChange={(e)=>setName(e.target.value)} />  
+       <label htmlFor='card-number'>CARD NUMBER:</label>
+       <input id='card-number' value={cardNum} type='text' onChange={(e)=>setCardNum(e.target.value)}  placeholder='e.g 1234567890'/>
+      
+       <div className="flex-1">
+        <span><label htmlFor="expiry_date">Exp. Date </label>   <label htmlFor='cvc'>CVC</label>  </span>
+                    <input
+                      type="month"
+                      name="expiry_date"
+                      id="expiry_date"
+                      placeholder="MM YY"
+                      required
+                      value={cal}
+                      onChange={(e) => setCal(e.target.value)}
+                    />
+                  </div>
+             
+        <div>
+       
+                    
+       <label htmlFor='cvc'>CVC</label>
+      <input id='cvc' name='cvc' value={cvc}  type='number' onChange={(e)=>setCvc(e.target.value)} placeholder='e.g 123'/>
+      </div>   
+       <br></br>
+       <div>
        <button  type='submit'>confirm</button>
+       </div>
       </div>
-      
-      
-      
       </form> 
-      
+      </section>
     </div>
   )
 }
